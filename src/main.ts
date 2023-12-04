0c97b5e0740fec9a7f90a7d8168df5ac5aca3e99
ZZZoyi <liuzhuoyi@uino.com>
2023-12-04 18:07:38 +0800

    style: elementui添加暗黑模式，添加sass

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import router from './router/index'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import './style.css'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './assets/styles/element/index.scss'
 import App from './App.vue'
 
 const app = createApp(App)

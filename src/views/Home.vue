4458fe655c264e1d9757c3729d9db2249cdcc66d
ZZZoyi <liuzhuoyi@uino.com>
2023-12-08 17:01:00 +0800

    feat: 添加Teleport和Transition示例

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,17 +8,22 @@
       <pre>{{ data }}</pre>
     </div>
     <div v-else>Loading...</div>
-    <CustomDirectives></CustomDirectives>
+    <el-button @click="handleShow(true)">显示组件</el-button>
+    <el-button @click="handleShow(false)">隐藏组件</el-button>
+    <Transition name="fade">
+      <CustomDirectives v-show="show"></CustomDirectives>
+    </Transition>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, watch, watchEffect, inject, provide } from 'vue'
+import { ref, watch, watchEffect, inject, provide, h } from 'vue'
 import { useFetch } from '../hooks/useFetch'
 import CustomDirectives from '../components/CustomDirectives.vue'
 import { messageInjectionKey } from '../symbols/homeKeys'
 
 const count = ref(0)
+const show = ref(false)
 const { data, error } = useFetch('https://jsonplaceholder.typicode.com/todos/1')
 
 provide(messageInjectionKey, data)
@@ -37,8 +42,20 @@ watchEffect(() => {
 const handleCount = () => {
   count.value ++
 }
+const handleShow = (boolean: boolean = false) => {
+  show.value = boolean
+}
+
 </script>
 
 <style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 3s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
---
+++ src/views/Outer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="outer">
+    <h3>Tooltips with Vue 3 Teleport</h3>
+    <div>
+      <MyModay />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MyModay from '../components/MyModal.vue'
+
+</script>
+
+<style scoped>
+
+</style>
